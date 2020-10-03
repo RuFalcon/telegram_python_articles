7d73c76a8397b85997a300c12cf2d92436e990a5
Maxim <SokolovMN123@yandex.ru>
2020-10-03 19:33:33 +0300

    Fix telegram api key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_API_TOKEN = str(os.getenv('SLACK_API_TOKEN'))
+TELEGRAM_API_TOKEN = str(os.getenv('TELEGRAM_API_TOKEN'))
 CHAT_ID = str(os.getenv('CHAT_ID'))
 DATABASE = str(os.getenv('DATABASE'))
 PGUSER = str(os.getenv('PGUSER'))
